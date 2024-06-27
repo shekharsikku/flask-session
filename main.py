6cfb15fa8869a5864b9ec85cd76bf93dd297dfac
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-06-27 09:10:47 +0530

    chore(port): access port via env variable for run application

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from logging import basicConfig, DEBUG
 from src.app import app, socketio
+from src.utils import envs
 
 
 if __name__ == "__main__":
     try:
         basicConfig(level=DEBUG)
-        socketio.run(app=app, host="localhost", port=4000, debug=True)
+        socketio.run(app=app, host="localhost", port=envs["PORT"], debug=True)
     except Exception as e:
         print(f"Error: {e}")
         
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from src.app import app, socketio
+from src.utils import envs
 
 
 if __name__ == "__main__":
     try:
-        socketio.run(app=app, host="0.0.0.0", port=4000)
+        socketio.run(app=app, host="0.0.0.0", port=envs["PORT"])
     except Exception as e:
         print(f"Error: {e}")
         
