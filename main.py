a08c6ebc2761f01fcb64ea680d245ac55fdb509c
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-06-30 09:59:39 +0530

    feat(redis): redis configuration for store session and data caching

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from src.utils import envs
 
 if __name__ == "__main__":
     try:
-        basicConfig(level=DEBUG)
-        socketio.run(app=app, host="localhost", port=envs["PORT"], debug=True)
+        is_development = envs["SERVER_MODE"] == "development"
+        if is_development:
+            basicConfig(level=DEBUG)
+        socketio.run(app=app, host="localhost", port=envs["PORT"], debug=is_development)
     except Exception as e:
         print(f"Error: {e}")
         
